4d2294a740e70292640abb48a29fe1caf657de69
Vincent Iadevaia <vincentiadevaia@gmail.com>
2016-01-24 21:29:44 +0000

    Updates

--- js/masonry.js
+++ js/masonry.js
@@ -11,8 +11,14 @@ $(document).ready(function () {
     $mason.masonry('layout');
   });
 
-  $("#container").fadeIn(3000);
+  $("#container").fadeIn(2000);
 
+  // $('nav').on('click', 'a', function(e){
+  //   e.preventDefault();
+  //   var url = e.currentTarget.href;
+  //   $('#container').load(url+'#container .item');
+  //   history.pushState({},url,url);
+  // });  
 });
 
 
